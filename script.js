d84420e8be4bcf47d7ea4029fa7027161db878d8
Xavian Moody <XavianMoody1025@gmail.com>
2023-12-10 21:58:19 -0500

    Finished

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 "use strict";
-function toggleSlider() {
-  const sliderInner = document.querySelector(".slider-inner");
-  let scrollStart = 0;
 
-  function moveSlide(e) {
+function toggleSlider() {
+  const slider = document.querySelector(".slider");
+  let oldx = 0;
+
+  // Get mouse movement direction
+  function getMouseDirection(e) {
+    let direction;
+
+    if (e.pageX < oldx) {
+      direction = "left";
+    } else if (e.pageX > oldx) {
+      direction = "right";
+    }
+
+    oldx = e.pageX;
+
+    return direction;
+  }
+
+  // Start Dragging
+  function dragging(e) {
+    let direction = getMouseDirection(e);
+
     if (e.buttons === 1) {
-      sliderInner.style.left = `-${(scrollStart += 1)}px`;
+      if (direction === "left") {
+        slider.scrollLeft -= e.movementX;
+      } else if (direction === "right") {
+        slider.scrollLeft -= e.movementX;
+      }
+      console.log(direction);
     }
   }
 
-  sliderInner.addEventListener("mousemove", moveSlide);
+  slider.addEventListener("mousemove", dragging);
 }
 
 toggleSlider();
